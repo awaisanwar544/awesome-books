21d7e17253599c52e411912d2c365654af2bdccd
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-02-07 22:01:14 +0100

    Add new book to my awesome books

--- main.js
+++ main.js
@@ -1,37 +1,56 @@
 let books = [
   {
-    title: 'The Tiral',
-    author: 'Franz Kafka',
+    title: "The Tiral",
+    author: "Franz Kafka",
   },
   {
-    title: 'Harry Potter',
-    author: 'J. K. Rowling',
+    title: "Harry Potter",
+    author: "J. K. Rowling",
   },
 ];
 
 // populate dom with the list
 
-const addBtn = document.querySelector('#add-btn');
-let removeBtn = document.querySelectorAll('.remove');
+const addBtn = document.querySelector("#add-btn");
+let removeBtn = document.querySelectorAll(".remove");
 
 function populateDom(list) {
-  const booksListDiv = document.querySelector('#books-list');
+  const booksListDiv = document.querySelector("#books-list");
   for (let i = 0; i < list.length; i += 1) {
-    const div = document.createElement('div');
-    const pTitle = document.createElement('p');
+    const div = document.createElement("div");
+    const pTitle = document.createElement("p");
     pTitle.innerHTML = list[i].title;
     div.appendChild(pTitle);
-    const pAuthor = document.createElement('p');
+    const pAuthor = document.createElement("p");
     pAuthor.innerHTML = list[i].author;
     div.appendChild(pAuthor);
-    const btn = document.createElement('button');
-    btn.className = 'remove';
-    btn.innerHTML = 'Remove';
+    const btn = document.createElement("button");
+    btn.className = "remove";
+    btn.innerHTML = "Remove";
     div.appendChild(btn);
-    const hr = document.createElement('hr');
+    const hr = document.createElement("hr");
     div.appendChild(hr);
     booksListDiv.appendChild(div);
   }
-  removeBtn = document.querySelectorAll('.remove');
+  removeBtn = document.querySelectorAll(".remove");
   addEvents();
 }
+
+// add book to the list
+
+function addBook() {
+  const bookTitle = document.querySelector("#title").value;
+  const bookAuthor = document.querySelector("#author").value;
+  if (bookTitle && bookAuthor) {
+    const book = {
+      title: bookTitle,
+      author: bookAuthor,
+    };
+    books.push(book);
+    populateDom([book]);
+    addToLocalStorage(books);
+    document.querySelector("form").reset();
+  }
+}
+
+addBtn.addEventListener("click", addBook);
