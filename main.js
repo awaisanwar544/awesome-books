c479709c38115014e9cbf20610ef346d9e063b56
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-02-09 16:17:07 +0100

    Add date function to my awesome book

--- main.js
+++ main.js
@@ -7,19 +7,19 @@ class Book {
 
   static books = [
     {
-      id: '001',
-      title: 'The Trial',
-      author: 'Franz Kafka',
+      id: "001",
+      title: "The Trial",
+      author: "Franz Kafka",
     },
     {
-      id: '002',
-      title: 'Harry Potter',
-      author: 'J. K. Rowling',
+      id: "002",
+      title: "Harry Potter",
+      author: "J. K. Rowling",
     },
   ];
 
   static addToLocalStorage(data) {
-    localStorage.setItem('data', JSON.stringify(data));
+    localStorage.setItem("data", JSON.stringify(data));
   }
 
   static addToBooks(book) {
@@ -35,11 +35,11 @@ class Book {
 }
 
 // add event listner to newly added book remove button
-let removeBtn = document.querySelectorAll('.remove');
+let removeBtn = document.querySelectorAll(".remove");
 
 function addEvents() {
   removeBtn.forEach((element) => {
-    element.addEventListener('click', (e) => {
+    element.addEventListener("click", (e) => {
       const ref = e.target.id;
       Book.removeBook(ref);
       e.target.parentElement.remove();
@@ -49,47 +49,82 @@ function addEvents() {
 
 // populate dom with the list
 
-const addBtn = document.querySelector('#add-btn');
+const addBtn = document.querySelector("#add-btn");
 
 function populateDom(list) {
-  const booksListDiv = document.querySelector('#books-list');
+  const booksListDiv = document.querySelector("#books-list");
   for (let i = 0; i < list.length; i += 1) {
-    const div = document.createElement('div');
-    div.className = 'book';
-    const pTitle = document.createElement('p');
+    const div = document.createElement("div");
+    div.className = "book";
+    const pTitle = document.createElement("p");
     pTitle.innerHTML = `"${list[i].title}" by ${list[i].author}`;
     div.appendChild(pTitle);
-    const btn = document.createElement('button');
-    btn.className = 'remove';
-    btn.innerHTML = 'Remove';
+    const btn = document.createElement("button");
+    btn.className = "remove";
+    btn.innerHTML = "Remove";
     btn.id = list[i].id;
     div.appendChild(btn);
     booksListDiv.appendChild(div);
   }
-  removeBtn = document.querySelectorAll('.remove');
+  removeBtn = document.querySelectorAll(".remove");
   addEvents();
 }
 
+function nav(element, link) {
+  document.querySelector(".show").className = "hidden";
+  document.querySelector(element).className = "show";
+  document.querySelectorAll("a").forEach((item) => {
+    item.className = "";
+  });
+  link.className = "active";
+}
 // get input from form and call addToBooks method
 
 function addNewBook() {
-  const bookTitle = document.querySelector('#title').value;
-  const bookAuthor = document.querySelector('#author').value;
+  const bookTitle = document.querySelector("#title").value;
+  const bookAuthor = document.querySelector("#author").value;
   if (bookTitle && bookAuthor) {
     const newBook = new Book(bookTitle, bookAuthor);
     Book.addToBooks(newBook);
     populateDom([newBook]);
-    document.querySelector('form').reset();
+    document.querySelector("form").reset();
+    const listLink = document.querySelector("#list-link");
+    nav("#books", listLink);
   }
 }
 
-addBtn.addEventListener('click', addNewBook);
+addBtn.addEventListener("click", addNewBook);
+
+document.querySelectorAll("a").forEach((item) => {
+  const ref = item.getAttribute("href");
+  item.addEventListener("click", () => {
+    nav(ref, item);
+  });
+});
+
+const dateTime = () => {
+  const time = document.querySelector(".date");
+  const now = new Date();
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+  const locale = navigator.en;
+  time.innerHTML = new Intl.DateTimeFormat(locale, options).format(now);
+
+  setTimeout(dateTime, 1000);
+};
+dateTime();
 
 window.onload = () => {
-  if (localStorage.getItem('data') === null) {
+  if (localStorage.getItem("data") === null) {
     populateDom(Book.books);
   } else {
-    const localBooks = JSON.parse(localStorage.getItem('data'));
+    const localBooks = JSON.parse(localStorage.getItem("data"));
     populateDom(localBooks);
     Book.books = localBooks;
   }
